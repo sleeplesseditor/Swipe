39323038299e6af1688d69da9ee1cd0453b9f311
sleeplesseditor <josh@bryson.me.uk>
2018-05-18 13:09:22 +0100

    Deck component setup

---
+++ src/Deck.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Animated, View } from 'react-native';
+
+class Deck extends Component {
+    render() {
+        return (
+            <View />
+        );
+    }
+}
+
+export default Deck;
